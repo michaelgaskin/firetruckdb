65dbbf1ea0a8f657aa71dd686151a4d9a3d16fb6
michaelgaskin <43794091+michaelgaskin@users.noreply.github.com>
2024-04-03 12:54:15 -0400

    Update index.js
    Remove extraneous reference to firetruck model

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,6 @@ const port = 3000;
 
 const firetruckRoutes = require('./routes/firetruck.routes');
 
-const firetruck = require('./models/firetruck');
-
 app.use('/firetrucks', firetruckRoutes);
 
 app.listen(port, () => {
