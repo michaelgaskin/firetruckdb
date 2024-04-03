bc4e823592494e9fd0ff1d9961f32da158ff5203
michaelgaskin <michael.gaskin@outlook.com>
2024-04-03 16:17:51 +0000

    Setup mongodb connection string

--- backend/Models/Firetruck.js
+++ backend/Models/Firetruck.js
@@ -1,4 +1,15 @@
 const mongoose = require('mongoose');
+const mongoDBConnectionString = mongodb+srv://mikegaskin01:a9dgnRw4lpXjpWTi@firetruckdb-mongo.92nx62r.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', () => {
+  console.log('MongoDB connected');
+});
 const Schema = mongoose.Schema;
 let firetruckSchema = new Schema({
   year : {
