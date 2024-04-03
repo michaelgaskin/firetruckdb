510132d2a86e4e808fd4408bc36d3dfa6b03863a
michaelgaskin <michael.gaskin@outlook.com>
2024-04-03 12:57:11 +0000

    Added mongoose to routes

--- backend/Routes/Firetruck.routes.js
+++ backend/Routes/Firetruck.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Firetruck = require('../models/Firetruck');
 
 // Create a new firetruck
